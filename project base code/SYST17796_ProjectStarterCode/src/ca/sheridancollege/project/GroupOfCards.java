774e13cebe425cf02a1706bc86bb2ce887b55041
viraj1911 <padhiyar@sheridancollage.ca>
2019-03-12 22:07:41 -0400

    Virajkumar Padhiyar Commit this

---
+++ project base code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/GroupOfCards.java
@@ -0,0 +1,23 @@
+/**
+ * SYST 17796 Project Winter 2019 Base code.
+ * Students can modify and extend to implement their game.
+ * Virajkumar Padhiyar
+ * Jeni Patel
+ * Meet Patel
+ * Parthkumar Patel
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * A concrete class that represents any grouping of cards for a Game.
+ * HINT, you might want to subclass this more than once.
+ * The group of cards has a maximum size attribute which is flexible for reuse.
+ * 
+ */
+public class GroupOfCards 
+{
+    
+}//end class
---
+++ project base code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Player.java
@@ -0,0 +1,18 @@
+/**
+ * SYST 17796 Project Winter 2019 Base code.
+ * Students can modify and extend to implement their game.
+ * Virajkumar Padhiyar
+ * Jeni Patel
+ * Meet Patel
+ * Parthkumar Patel
+ */
+package ca.sheridancollege.project;
+
+/**
+ * A class that models each Player in the game. Players have an identifier, which should be unique.
+ * @author dancye, 2018
+ */
+public abstract class Player 
+{
+    
+}
